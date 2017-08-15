950fd70599677cf47a135b1ffb57fafa77efe670
MIKOLAJ63 <MIKOLAJ63>
2017-08-15 12:09:14 +0200

    1st js + comments

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//scripts.js
+var name=prompt('Enter your name');
+alert('Hello, ' + name);//To 1-sze wywolanie funkcji alert
+/*Tym blokowym komentarzem 
+block comment console.log
+przerywamy wywolanie console.log*/
+console.log('Hello, ' + name);
+/*Jeszcze jeden komentarz blokowy na wszelki wypadek*/
